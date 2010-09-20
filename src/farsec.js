9e1bd3bd67a534df4699a3c9ef9fe7a926af5edd
Walter Tetzner <walter@waltertetzner.net>
2010-09-20 18:33:59 -0400

    Added license to the top of farsec.js

--- src/farsec.js
+++ src/farsec.js
@@ -1,4 +1,25 @@
+/*
+Copyright (c) 2010 Walter Tetzner
 
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 // Returns a parser that parses a string
 function string(tok)
@@ -11,7 +32,7 @@ function string(tok)
                     parsed: tok,
                     ast: null};
         }
-        
+
         if(input.substr(0, tok.length) == tok)
         {
             return {rest: input.substr(tok.length, input.length - tok.length),
@@ -259,7 +280,7 @@ function sepBy(parser, separator)
         obj.ast.push(firstParse.ast);
         obj.rest = firstParse.rest;
         obj.parsed = obj.parsed + firstParse.parsed;
-        
+
         var rest = (many(and(separator, parser)))(obj.rest);
         if(rest != false)
         {
@@ -274,7 +295,7 @@ function sepBy(parser, separator)
             obj.rest = rest.rest;
             obj.parsed = obj.parsed + rest.parsed;
         }
-        
+
         return obj;
     }
 }
