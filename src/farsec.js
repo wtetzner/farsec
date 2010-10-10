b2fdbea142df9b075e79fa70b418d203290cd6ce
Walter Tetzner <walter@waltertetzner.net>
2010-10-10 13:51:44 -0400

    Reindented farsec.js.

--- src/farsec.js
+++ src/farsec.js
@@ -1,323 +1,321 @@
 /*
-Copyright (c) 2010 Walter Tetzner
+  Copyright (c) 2010 Walter Tetzner
 
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
 
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 // Returns a parser that parses a string
 function string(tok)
 {
-    return function(input)
+  return function(input)
+  {
+    if(tok == "")
     {
-        if(tok == "")
-        {
-            return {rest: input,
-                    parsed: tok,
-                    ast: null};
-        }
+      return {rest: input,
+              parsed: tok,
+              ast: null};
+    }
 
-        if(input.substr(0, tok.length) == tok)
-        {
-            return {rest: input.substr(tok.length, input.length - tok.length),
-                    parsed: tok,
-                    ast: tok};
-        }
-        else
-        {
-            return false;
-        }
-    };
+    if(input.substr(0, tok.length) == tok)
+    {
+      return {rest: input.substr(tok.length, input.length - tok.length),
+              parsed: tok,
+              ast: tok};
+    }
+    else
+    {
+      return false;
+    }
+  };
 }
 
 // Returns a parser that parses a single letter
 function letter()
 {
-    return function(input)
+  return function(input)
+  {
+    if(input.charAt(0).search(/[a-zA-Z]/) != -1)
     {
-        if(input.charAt(0).search(/[a-zA-Z]/) != -1)
-        {
-            return {rest: input.substr(1, input.length - 1),
-                    parsed: input.charAt(0),
-                    ast: input.charAt(0)};
-        }
-        else
-        {
-            return false;
-        }
-    };
+      return {rest: input.substr(1, input.length - 1),
+              parsed: input.charAt(0),
+              ast: input.charAt(0)};
+    }
+    else
+    {
+      return false;
+    }
+  };
 }
 
 // Returns a parser that parses a single digit
 function digit()
 {
-    return function(input)
+  return function(input)
+  {
+    if(input.charAt(0).search(/[0-9]/) != -1)
     {
-        if(input.charAt(0).search(/[0-9]/) != -1)
-        {
-            return {rest: input.substr(1, input.length - 1),
-                    parsed: input.charAt(0),
-                    ast: input.charAt(0)};
-        }
-        else
-        {
-            return false;
-        }
-    };
+      return {rest: input.substr(1, input.length - 1),
+              parsed: input.charAt(0),
+              ast: input.charAt(0)};
+    }
+    else
+    {
+      return false;
+    }
+  };
 }
 
 // Returns a parser that parses any character that's not in the input
 function noneOf(chars)
 {
-    return function(input)
+  return function(input)
+  {
+    var match = new RegExp('[^' + chars + ']');
+    if(input.charAt(0).search(match) != -1)
     {
-        var match = new RegExp('[^' + chars + ']');
-        if(input.charAt(0).search(match) != -1)
-        {
-            return {rest: input.substr(1, input.length - 1),
-                    parsed: input.charAt(0),
-                    ast: input.charAt(0)};
-        }
-        else
-        {
-            return false;
-        }
-    };
+      return {rest: input.substr(1, input.length - 1),
+              parsed: input.charAt(0),
+              ast: input.charAt(0)};
+    }
+    else
+    {
+      return false;
+    }
+  };
 }
 
 // Returns a parser that parses any character that's in the input
 function oneOf(chars)
 {
-    return function(input)
+  return function(input)
+  {
+    var match = new RegExp('[' + chars + ']');
+    if(input.charAt(0).search(match) != -1)
     {
-        var match = new RegExp('[' + chars + ']');
-        if(input.charAt(0).search(match) != -1)
-        {
-            return {rest: input.substr(1, input.length - 1),
-                    parsed: input.charAt(0),
-                    ast: input.charAt(0)};
-        }
-        else
-        {
-            return false;
-        }
-    };
+      return {rest: input.substr(1, input.length - 1),
+              parsed: input.charAt(0),
+              ast: input.charAt(0)};
+    }
+    else
+    {
+      return false;
+    }
+  };
 }
 
 // Returns the first successful parse
 function or()
 {
-    var items = arguments;
-    return function(input)
+  var items = arguments;
+  return function(input)
+  {
+    for(var i = 0; i < items.length; i++)
     {
-        for(var i = 0; i < items.length; i++)
-        {
-            var result = items[i](input);
-            if(result != false)
-            {
-                return result;
-            }
+      var result = items[i](input);
+      if(result != false)
+      {
+        return result;
+      }
         }
-        return false;
-    };
+    return false;
+  };
 }
 
 // Returns a parser that parses 1 or more times using the parser passed in
 function many1(parser)
 {
-    return function(input)
+  return function(input)
+  {
+    var value = {rest: input, parsed: '', ast: []};
+    for(var i = 0; true; i++)
     {
-        var value = {rest: input, parsed: '', ast: []};
-        for(var i = 0; true; i++)
+      var result = parser(value.rest);
+      if(result != false)
+      {
+        if(result.ast != null)
         {
-            var result = parser(value.rest);
-            if(result != false)
-            {
-                if(result.ast != null)
-                {
-                    value.ast.push(result.ast);
-                }
-                value = {rest: result.rest,
-                         parsed: value.parsed + result.parsed,
-                         ast: value.ast};
-            }
-            else
-            {
-                if(value.parsed == '')
-                {
-                    return false;
-                }
-                else
-                {
-                    break;
-                }
-            }
+          value.ast.push(result.ast);
         }
-        return value;
-    };
+            value = {rest: result.rest,
+                     parsed: value.parsed + result.parsed,
+                     ast: value.ast};
+      }
+      else
+      {
+        if(value.parsed == '')
+        {
+          return false;
+        }
+        else
+        {
+          break;
+        }
+      }
+    }
+    return value;
+  };
 }
 
 // Returns a parser that parses 0 or more times using the parser passed in
 function many(parser)
 {
-    return function(input)
+  return function(input)
+  {
+    var value = {rest: input, parsed: '', ast: []};
+    for(var i = 0; true; i++)
     {
-        var value = {rest: input, parsed: '', ast: []};
-        for(var i = 0; true; i++)
+      var result = parser(value.rest);
+      if(result != false)
+      {
+        if(result.ast != null)
         {
-            var result = parser(value.rest);
-            if(result != false)
-            {
-                if(result.ast != null)
-                {
-                    value.ast.push(result.ast);
-                }
-                value = {rest: result.rest,
-                         parsed: value.parsed + result.parsed,
-                         ast: value.ast};
-            }
-            else
-            {
-                if(value.parsed == '')
-                {
-                    value = {rest: value.rest,
-                             parsed: '',
-                             ast: null};
-                }
-                break;
-            }
+          value.ast.push(result.ast);
         }
-        return value;
-    };
+        value = {rest: result.rest,
+                 parsed: value.parsed + result.parsed,
+                 ast: value.ast};
+      }
+      else
+      {
+        if(value.parsed == '')
+        {
+          value = {rest: value.rest,
+                   parsed: '',
+                   ast: null};
+        }
+        break;
+      }
+    }
+    return value;
+  };
 }
 
 // Returns a parser that runs each parser in order
 function and()
 {
-    var items = arguments;
-    return function(input)
+  var items = arguments;
+  return function(input)
+  {
+    var value = {rest: input, parsed: '', ast: []};
+    for(var i = 0; i < items.length; i++)
     {
-        var value = {rest: input, parsed: '', ast: []};
-        for(var i = 0; i < items.length; i++)
+      var result = items[i](value.rest);
+      if(result != false)
+      {
+        var rst = result.rest;
+        if(result.ast != null)
         {
-            var result = items[i](value.rest);
-            if(result != false)
-            {
-                var rst = result.rest;
-                if(result.ast != null)
-                {
-                    value.ast.push(result.ast);
-                }
-                value = {rest: rst,
-                         parsed: value.parsed + result.parsed,
-                         ast: value.ast};
-            }
-            else
-            {
-                return false;
-            }
+          value.ast.push(result.ast);
         }
-        return value;
-    };
+        value = {rest: rst,
+                 parsed: value.parsed + result.parsed,
+                 ast: value.ast};
+      }
+      else
+      {
+        return false;
+      }
+    }
+    return value;
+  };
 }
 
 // Takes the result of a parser and copies the parsed string to ast
 function makeStr(parser)
 {
-    return function(input)
+  return function(input)
+  {
+    var result = parser(input);
+    if(result != false)
     {
-        var result = parser(input);
-        if(result != false)
-        {
-            if(result.parsed != '')
-            {
-                return {rest: result.rest,
-                        parsed: result.parsed,
-                        ast: result.parsed};
-            }
-            else
-            {
-                return {rest: result.rest,
-                        parsed: result.parsed,
-                        ast: null};
-            }
-        }
-        else
-        {
-            return false;
-        }
-    };
+      if(result.parsed != '')
+      {
+        return {rest: result.rest,
+                parsed: result.parsed,
+                ast: result.parsed};
+      }
+      else
+      {
+        return {rest: result.rest,
+                parsed: result.parsed,
+                ast: null};
+      }
+    }
+    else
+    {
+      return false;
+    }
+  };
 }
 
 // Returns a parser that parses a list of items separated by a list of other items
 function sepBy(parser, separator)
 {
-    return function(input)
+  return function(input)
+  {
+    var obj = {rest: input, parsed: '', ast: []};
+    var firstParse = parser(obj.rest);
+    if(firstParse == false)
     {
-        var obj = {rest: input, parsed: '', ast: []};
-        var firstParse = parser(obj.rest);
-        if(firstParse == false)
-        {
-            obj.ast = null;
-            return obj;
-        }
-        obj.ast.push(firstParse.ast);
-        obj.rest = firstParse.rest;
-        obj.parsed = obj.parsed + firstParse.parsed;
+      obj.ast = null;
+      return obj;
+    }
+    obj.ast.push(firstParse.ast);
+    obj.rest = firstParse.rest;
+    obj.parsed = obj.parsed + firstParse.parsed;
 
-        var rest = (many(and(separator, parser)))(obj.rest);
-        if(rest != false)
+    var rest = (many(and(separator, parser)))(obj.rest);
+    if(rest != false)
+    {
+      if(rest.ast != null)
+      {
+        for(var i = 0; i < rest.ast.length; i++)
         {
-            if(rest.ast != null)
-            {
-                for(var i = 0; i < rest.ast.length; i++)
-                {
-                    obj.ast.push(rest.ast[i][0]);
-                    obj.ast.push(rest.ast[i][1]);
-                }
-            }
-            obj.rest = rest.rest;
-            obj.parsed = obj.parsed + rest.parsed;
+          obj.ast.push(rest.ast[i][0]);
+          obj.ast.push(rest.ast[i][1]);
         }
+      }
+      obj.rest = rest.rest;
+      obj.parsed = obj.parsed + rest.parsed;
+    }
 
-        return obj;
-    };
+    return obj;
+  };
 }
 
 // Returns a parser that replaces a parser's ast
 function ret(parser, func)
 {
-    return function(input)
+  return function(input)
+  {
+    var result = parser(input);
+    if(result != false)
     {
-        var result = parser(input);
-        if(result != false)
-        {
-            return {rest: result.rest,
-                    parsed: result.parsed,
-                    ast: func(result.ast)};
-        }
+      return {rest: result.rest,
+              parsed: result.parsed,
+              ast: func(result.ast)};
+    }
         else
-        {
-            return false;
-        }
-    };
+    {
+      return false;
+    }
+  };
 }
-
-
 
